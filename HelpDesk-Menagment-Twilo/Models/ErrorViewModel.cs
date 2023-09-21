62c26b0c94dbc1f6cb18d4e9b0a496e41fa739ee
jakub <jakub@LAPTOP-KSEMUCDD>
2023-09-21 13:04:54 +0200

    Dodaj pliki projektów.

---
+++ HelpDesk-Menagment-Twilo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelpDesk_Menagment_Twilo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
