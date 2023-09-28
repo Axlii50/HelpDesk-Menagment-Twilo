fe1c52e35c4a44aff352189744918b74b984a9e3
Axlii50 <kubad4d@wp.pl>
2023-09-28 14:49:07 +0200

    LoginPage

--- HelpDesk-Menagment-Twilo/Controllers/HomeController.cs
+++ HelpDesk-Menagment-Twilo/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
-﻿using HelpDesk_Menagment_Twilo.Models;
+﻿using HelpDesk_Menagment_Twilo.Data;
+using HelpDesk_Menagment_Twilo.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 
 namespace HelpDesk_Menagment_Twilo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HelpDesk_Menagment_TwiloContext helpDeskContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, HelpDesk_Menagment_TwiloContext helpDesk_Menagment_TwiloContext)
         {
             _logger = logger;
+            helpDeskContext = helpDesk_Menagment_TwiloContext;
         }
 
         public IActionResult LoginPage()
@@ -22,6 +26,17 @@ namespace HelpDesk_Menagment_Twilo.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult Login([Bind("login,password")]string login, string password)
+        {
+            var account = helpDeskContext.Account.Where(acc => acc.Login == login && acc.Password == password).FirstOrDefault();
+
+            if(account == null)
+                return View("~/Views/Home/LoginPage.cshtml");
+            else
+                return View("~/Views/HelpDesk/Index.cshtml");
         }
     }
 }
