aafa6d6bf7516a34406d6791cd606d4103a7b845
jakub <jakub@LAPTOP-KSEMUCDD>
2023-12-30 00:16:05 +0100

    recognition

--- HelpDesk-Menagment-Twilo/Services/DeliveryRecognitionService.cs
+++ HelpDesk-Menagment-Twilo/Services/DeliveryRecognitionService.cs
@@ -1,5 +1,6 @@
 ﻿using HelpDesk_Menagment_Twilo.Interfaces;
 using HelpDesk_Menagment_Twilo.Models.DataBase.Package;
+using System.Text.RegularExpressions;
 
 namespace HelpDesk_Menagment_Twilo.Services
 {
@@ -7,7 +8,26 @@ namespace HelpDesk_Menagment_Twilo.Services
     {
         public DeliveryType Recognize(string PackageID)
         {
-            throw new NotImplementedException();
+            if (IsInpostTrackingNumber(PackageID)) return DeliveryType.Inpost;
+            if (IsAllegroOneTrackingNumber(PackageID)) return DeliveryType.AllegroOne;
+            if (IsOrlenTrackingNumber(PackageID)) return DeliveryType.Orlen;
+
+            return 0;
+        }
+
+        bool IsInpostTrackingNumber(string trackingNumber)
+        {
+            return Regex.IsMatch(trackingNumber, @"^6319\d{20}$");
+        }
+
+        bool IsAllegroOneTrackingNumber(string trackingNumber)
+        {
+            return Regex.IsMatch(trackingNumber, @"^A[A-Z0-9]{9}$");
+        }
+
+        bool IsOrlenTrackingNumber(string trackingNumber)
+        {
+            return Regex.IsMatch(trackingNumber, @"^21\d{11}$");
         }
     }
 }
