46fc389538bfefc310597d24ec380ef45d927097
Axlii50 <kubad4d@wp.pl>
2024-01-09 01:03:07 +0100

    Dodanie fragmentu kodu do generowania paczek

--- HelpDesk-Menagment-Twilo/Services/BackGroundService.cs
+++ HelpDesk-Menagment-Twilo/Services/BackGroundService.cs
@@ -1,6 +1,32 @@
-﻿namespace HelpDesk_Menagment_Twilo.Services
+﻿using HelpDesk_Menagment_Twilo.Interfaces;
+
+namespace HelpDesk_Menagment_Twilo.Services
 {
-    public class BackGroundService
+    public class BackGroundService : BackgroundService
     {
+        private readonly IEnumerable<IBackGroundService> _myServices;
+
+        public BackGroundService(IEnumerable<IBackGroundService> myServices)
+        {
+            _myServices = myServices;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                // Tutaj umieść kod, który ma być wykonany w określonym interwale
+                Console.WriteLine("Wykonuję zadanie...");
+
+                // Przykładowe zadanie (możesz go dostosować do swoich potrzeb)
+                foreach (var myService in _myServices)
+                {
+                    await myService.StartServiceTask();
+                }
+
+                // Ustaw interwał czasowy
+                await Task.Delay(TimeSpan.FromHours(8), stoppingToken);
+            }
+        }
     }
 }
--- HelpDesk-Menagment-Twilo/Services/PackageGeneratorService.cs
+++ HelpDesk-Menagment-Twilo/Services/PackageGeneratorService.cs
@@ -1,21 +1,28 @@
-﻿using HelpDesk_Menagment_Twilo.Interfaces;
+﻿using HelpDesk_Menagment_Twilo.Data;
+using HelpDesk_Menagment_Twilo.Interfaces;
 
 namespace HelpDesk_Menagment_Twilo.Services
 {
     public class PackageGeneratorService : IBackGroundService
     {
         private readonly IShippingService _shippingService;
-        private readonly IOrderService _rderService;
+        private readonly IOrderService _orderService;
+        private readonly HelpDesk_Menagment_TwiloContext context;
 
-        public PackageGeneratorService(IShippingService shippingService, IOrderService orderService)
+        public PackageGeneratorService(IShippingService shippingService, IOrderService orderService, HelpDesk_Menagment_TwiloContext context)
         {
             _shippingService = shippingService;
             _orderService = orderService;
-        } 
+            this.context = context;
+        }
 
         public async Task StartServiceTask()
         {
-            
+            //Download all orders new/Realization
+
+            //Sort out all that are already in database
+
+            //Generate number for rest
         }
     }
 }
